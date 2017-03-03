8b04e750067afa263d509a60a73acd28726eab3d
Issa Nimaga <issa.m.nimaga@gmail.com>
2017-03-03 20:18:00 +0000

    Restricted movement of protagonist to prevent going offscreen

--- Main.java
+++ Main.java
@@ -26,12 +26,6 @@ public class Main
 	
 	GameArena alpha = new GameArena (900, 800);
 	
-/*	Ball yolanda = new Ball(0,0,0,"BLUE");
-	Ball lelouche = new Ball(0,0,0, "GREEN");
-	alpha.addBall(yolanda);
-	alpha.addBall(lelouche);
-	*/
-	
 	alpha.addRectangle(head);	//Addition of protagonist begins here
 	alpha.addRectangle(body);
 	alpha.addRectangle(leg1);
@@ -53,14 +47,14 @@ public class Main
 	}
 	
 	//Code handling the movement of the protagonist begins here
-	if (alpha.rightPressed()){
+	if (alpha.rightPressed() && body.getXPosition() != 870){
 		
 		head.setXPosition((head.getXPosition() + 1));
 		body.setXPosition((body.getXPosition() + 1));
 		leg1.setXPosition((leg1.getXPosition() + 1));
 		leg2.setXPosition((leg2.getXPosition() + 1));
 	}
-	else if (alpha.leftPressed()){
+	else if (alpha.leftPressed() && body.getXPosition() != 30){
 		
 		head.setXPosition((head.getXPosition() - 1));
 		body.setXPosition((body.getXPosition() - 1));
@@ -89,59 +83,7 @@ public class Main
 		alpha.removeBall(bullet);
 
 	}
-	
-	
 	}
-	
-	
-	/* 
-	Random case1 = new Random();
-	int f = case1.nextInt(882) + 1;
-	beta.setXPosition(f);
-	int g = case1.nextInt(782) + 1;
-	beta.setYPosition(g);
-	
-	
-	while(c == 0){	//c=0; c < 100 ; c++
-	
-	if (directionX == 1) {	//Ball moving to the right
-		b = beta.getXPosition() + 1;
-		beta.setXPosition(b);
-	}
-	else if (directionX == 2) {  //Ball moving to the left
-		b = beta.getXPosition() - 1;
-		beta.setXPosition(b);
-	}
-	if (directionY == 1) {  //Ball moving downwards
-		e = beta.getYPosition() + 1;
-		beta.setYPosition(e);
-	}
-	else if (directionY == 2) {  //Ball moving upwards
-		e = beta.getYPosition() - 1;
-		beta.setYPosition(e);
-	}
-	
-	// The Code below sets the direction of the ball
-	
-	if (b == 882) {
-		directionX = 2;
-	}
-	else if (b == 18) {
-		directionX = 1;
-	}
-	
-	if (e == 782){
-		directionY = 2;
-	}
-	else if (e == 18) {
-		directionY = 1;
-	}
-	double h = b % 8; //this is to speed up the animation. By reducing the frequency at which this code is played
-	if (h == 0){
-		alpha.pause();
-	}
-	}
-*/
 	
 	}
 }
